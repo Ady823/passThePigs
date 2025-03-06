5a1cac5aff2069b98da233ab46765732e45da746
Ady823 <aduval@brooksschool.org>
2025-03-06 16:59:36 +0000

    implemented classes and player functions

---
+++ Players.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Players {
+    private String name;
+    private String strategy;
+    public Player(String name) {
+        this.name = name;
+    }
+    public boolean wantsToRoll(int myScore, int handScore, ArrayList<Integer> otherScores, int winningScore){
+        boolean roll = false;
+
+        return roll;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getStrategy(){
+        String strat = " ";
+        return strat;
+    }
+}
---
+++ Robots.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Robots extends Players {
+    private String name;
+private String strategy;
+public Player(String name)
+public boolean wantsToRoll(int myScore, int handScore, ArrayList<Integer> otherScores, int winningScore)
+public String getName()
+public String getStrategy()
+}
